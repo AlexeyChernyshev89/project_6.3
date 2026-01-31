8e9728a0343b6c099730aa0a0b023bde8b545c5d
AlexeyChernyshev89 <93157199+AlexeyChernyshev89@users.noreply.github.com>
2026-01-31 19:42:19 +0300

    исправил CompleteOrderTest.java. Убрал паузу и сделал 1 тест вместо двух -Добавил класс FaqData.java -Обновил HomePage.java с методами для FAQ -новый тест для FAQ

--- src/test/java/com/yandex/scooter/config/TestBase.java
+++ src/test/java/com/yandex/scooter/config/TestBase.java
@@ -1,36 +1,45 @@
 package com.yandex.scooter.config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
 public class TestBase {
     protected WebDriver driver;
+    private String browser = System.getProperty("browser", "chrome");
 
     @BeforeEach
     public void setUp() {
-        String browser = System.getProperty("browser", "chrome");
-
-        if ("firefox".equalsIgnoreCase(browser)) {
-            driver = new FirefoxDriver();
-        } else {
-            driver = new ChromeDriver();
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(chromeOptions);
+                break;
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://qa-scooter.praktikum-services.ru/");
 
-        // Ждем загрузки страницы
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        // Закрываем баннер с куками на главной странице
+        com.yandex.scooter.pages.HomePage homePage =
+                new com.yandex.scooter.pages.HomePage(driver);
+        homePage.closeCookieBannerIfPresent();
     }
 
     @AfterEach
